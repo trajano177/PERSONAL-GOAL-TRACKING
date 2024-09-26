8e45102e7fc89e72807985081d021e0c4e2e7525
Matheus Trajano <111097487+trajano177@users.noreply.github.com>
2024-09-25 23:16:55 -0300

    commit

--- server/.migrations/0001_even_morbius.sql
+++ server/.migrations/0001_even_morbius.sql
@@ -1,10 +1,13 @@
 CREATE TABLE IF NOT EXISTS "goal_completions" (
-	"id" text NOT NULL,
+	"id" text PRIMARY KEY NOT NULL,
+	"goal_id" text NOT NULL,
 	"created_at" timestamp with time zone DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+
+--> statement-breakpoint
 DO $$ BEGIN
- ALTER TABLE "goal_completions" ADD CONSTRAINT "goal_completions_id_goals_id_fk" FOREIGN KEY ("id") REFERENCES "public"."goals"("id") ON DELETE no action ON UPDATE no action;
+ ALTER TABLE "goal_completions" ADD CONSTRAINT "goal_completions_goal_id_goals_id_fk" FOREIGN KEY ("goal_id") REFERENCES "public"."goals"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
--- server/src/functions/create-goal-completion.ts
+++ server/src/functions/create-goal-completion.ts
@@ -1,5 +1,63 @@
-import { goalCompletions, goals } from "../db/schema"
-import { db } from "../db"
-import { count, lte , gte, sql, eq, and } from "drizzle-orm"
-import dayjs from "dayjs"
+import { count, and, lte, gte, eq, sql } from 'drizzle-orm'
+import { db } from '../db'
+import { goalCompletions, goals } from '../db/schema'
+import dayjs from 'dayjs'
 
+interface CreateGoalCompletionRequest {
+  goalId: string
+}
+
+export async function createGoalCompletion({
+  goalId,
+}: CreateGoalCompletionRequest) {
+  const lastDayOfWeek = dayjs().endOf('week').toDate()
+  const firstDayOfWeek = dayjs().startOf('week').toDate()
+
+  const goalsCompletionCounts = db.$with('goals_completion_counts').as(
+    db
+      .select({
+        goalId: goalCompletions.goalId,
+        completionsCount: count(goalCompletions.id).as('completionsCount'),
+      })
+      .from(goalCompletions)
+      .where(
+        and(
+          lte(goalCompletions.createdAt, lastDayOfWeek),
+          gte(goalCompletions.createdAt, firstDayOfWeek),
+          eq(goalCompletions.goalId, goalId)
+        )
+      )
+      .groupBy(goalCompletions.goalId)
+  )
+
+  const result = await db
+    .with(goalsCompletionCounts)
+    .select({
+      desiredWeeklyFrequency: goals.desiredWeeklyFrequency,
+      completionCount:
+        sql`COALESCE(${goalsCompletionCounts.completionsCount}, 0)`.mapWith(
+          Number
+        ),
+    })
+    .from(goals)
+    .leftJoin(goalsCompletionCounts, eq(goalsCompletionCounts.goalId, goals.id))
+    .where(eq(goals.id, goalId))
+    .limit(1)
+
+  const { completionCount, desiredWeeklyFrequency } = result[0]
+
+  if (completionCount >= desiredWeeklyFrequency) {
+    throw new Error('Goal already completed this week!')
+  }
+
+  const insertResult = await db
+    .insert(goalCompletions)
+    .values({
+      goalId,
+    })
+    .returning()
+
+  const goalCompletion = insertResult[0]
+
+  return { goalCompletion }
+}
