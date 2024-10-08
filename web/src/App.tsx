3154c044470e9bef6094edf64026aaa0db69ee96
Matheus Trajano <111097487+trajano177@users.noreply.github.com>
2024-10-07 21:26:16 -0300

    commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,53 +1,16 @@
-import {Plus, X} from 'lucide-react'
-import logo from './assets/logo-in-orbit.svg'
-import letsStart from './assets/lets-start-illustration.svg'
-import { Button } from './components/ui/button'
-import { Dialog, DialogClose, DialogContent, DialogDescription, DialogTitle, DialogTrigger } from './components/ui/dialog'
+
+import { Dialog } from './components/ui/dialog'
+import { CreateGoal } from './components/ui/create-goal'
+import { EmptyGoals } from './components/ui/empty-goals'
+
  export  function App() {
  return (
  <Dialog>
-   <div className="h-screen flex flex-col items-center justify-center gap-8">
-    <img src={logo} alt="in.orbit" />
-    <img src={letsStart} alt="" />
-    <p className='text-zinc-300 leading-relaxed max-w-80 text-center'>
-      Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
-      </p>
 
-     <DialogTrigger asChild>
-     <Button>
-      <Plus className='size-4'/>
-      Cadastrar meta
-      </Button>
-     </DialogTrigger>
-  </div>
-
-  <DialogContent>
-    <div className='flex flex-col gap-6'>
-    <div className='flex flex-col gap-3'>
-    <div className='flex items-center justify-between'>
-      <DialogTitle>Cadastrar meta</DialogTitle>
-      <DialogClose>
-        <X className='size-5 text-zinc-600'/>
-      </DialogClose>
-    </div>
-    <DialogDescription>
-    Adicione atividades que te fazem bem e que você quer continuar praticando toda semana.
-    </DialogDescription>
-    </div>
-    <form action="" className='flex-1 flex flex-col justify-between'>
-      <div>
-
-      </div>
-
-      <div className='flex items-center gap-3'>
-      <DialogClose asChild>
-      <Button className='flex-1' variant='secondary'>Fechar</Button>
-      </DialogClose>
-        <Button className='flex-1'>Salvar</Button>
-      </div>
-    </form>
-    </div>
-  </DialogContent>
+  <EmptyGoals />
+  <CreateGoal />
  </Dialog>
 )
 }
+
+// Summary
---
+++ web/src/components/ui/empty-goals.tsx
@@ -0,0 +1,25 @@
+import { Plus, X } from 'lucide-react'
+import logo from '../../assets/logo-in-orbit.svg'
+import letsStart from '../../assets/lets-start-illustration.svg'
+import { DialogTrigger } from './dialog'
+import { Button } from './button'
+
+
+export function EmptyGoals() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="" />
+      <p className='text-zinc-300 leading-relaxed max-w-80 text-center'>
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+      </p>
+
+      <DialogTrigger asChild>
+        <Button>
+          <Plus className='size-4' />
+          Cadastrar meta
+        </Button>
+      </DialogTrigger>
+    </div>
+  )
+}
