1c5a0d8dd114926f242702461f6a1c525951542f
Matheus Trajano <111097487+trajano177@users.noreply.github.com>
2024-11-18 20:38:34 -0300

    commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,13 +3,36 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/ui/create-goal'
 //import { EmptyGoals } from './components/ui/empty-goals'
 import { Summary } from './components/summary'
+import { useEffect, useState } from 'react'
+import { EmptyGoals } from './components/empty-goals'
 
+
+type SummaryResponse =  {
+  completed: number;
+  total: number;
+  goalsPerDay: Record<string, {
+      id: string;
+      title: string;
+      completedAt: string;
+  }[]>;
+}
 
  export  function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  useEffect(() => {
+    fetch('http://localhost:3333/summary')
+    .then(response =>{
+      return response.json()
+    }).then(data => {
+      setSummary(data.summary)
+    })
+  }, [])
  return (
  <Dialog>
 
-  <Summary />
+  {summary?.total && summary.total > 0 ?  <Summary /> : <EmptyGoals /> }
+ 
   <CreateGoal />
  </Dialog>
 )
