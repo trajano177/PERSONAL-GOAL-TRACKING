c1d4ddd075de53d30c698a3eb6d345f859ddbc7d
Matheus Trajano <111097487+trajano177@users.noreply.github.com>
2024-11-21 21:45:51 -0300

    commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,37 +1,25 @@
 
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/ui/create-goal'
-//import { EmptyGoals } from './components/ui/empty-goals'
 import { Summary } from './components/summary'
-import { useEffect, useState } from 'react'
 import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 
-type SummaryResponse =  {
-  completed: number;
-  total: number;
-  goalsPerDay: Record<string, {
-      id: string;
-      title: string;
-      completedAt: string;
-  }[]>;
-}
+
 
  export  function App() {
-  const [summary, setSummary] = useState<SummaryResponse | null>(null)
-
-  useEffect(() => {
-    fetch('http://localhost:3333/summary')
-    .then(response =>{
-      return response.json()
-    }).then(data => {
-      setSummary(data.summary)
-    })
-  }, [])
+ 
+  const {data} = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
  return (
  <Dialog>
 
-  {summary?.total && summary.total > 0 ?  <Summary /> : <EmptyGoals /> }
+  {data?.total && data.total > 0 ?  <Summary /> : <EmptyGoals /> }
  
   <CreateGoal />
  </Dialog>
--- web/src/components/summary.tsx
+++ web/src/components/summary.tsx
@@ -5,15 +5,36 @@ import { InOrbitIcon } from "./in-orbit-icon";
 import { Progress, ProgressIndicator } from "./ui/progress-bar";
 import { Separator } from "./ui/separator";
 import { OutlineButton } from "./ui/outline-button";
+import { useQuery } from "@tanstack/react-query";
+import { getSummary } from "../http/get-summary";
+import dayjs from "dayjs";
+import ptBR from "dayjs/locale/pt-BR"
+
+dayjs.locale(ptBR)
 
 export function Summary() {
+  const {data} = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
+
+  if (!data) {
+    return null
+  }
+
+  const firestDayOfWeek = dayjs().startOf('week').format(' D MMMM')
+  const lastDayOfWeek = dayjs().endOf('week').format(' D MMMM')
+
+  const completedPercentage = Math.round(data.completed * 100 / data.total)
+
   return (
     <div className="py-10 max-w-[480px] px-5 mx-auto flex flex-col gap-6">
 
       <div className="flex items-center justify-between">
       <div className="flex items-center gap-3">
       <InOrbitIcon />
-      <span className="text-lg font-semibold">5 a 10 de Agosto</span>
+      <span className="text-lg font-semibold capitalize">{firestDayOfWeek} - {lastDayOfWeek} </span>
       </div>
       <DialogTrigger asChild>
         <Button size="sm">
@@ -24,12 +45,12 @@ export function Summary() {
       </div>
       <div className="flex flex-col gap-3">
       <Progress value={8} max={15} >
-        <ProgressIndicator style={{width: '50%'}} />
+        <ProgressIndicator style={{width: `${completedPercentage}%`}} />
       </Progress>
 
       <div className="flex items-center justify-between text-xs text-zinc-400">
-        <span>Você completou <span className="text-zinc-100">8</span> de <span className="text-zinc-100">15</span> metas nessa semana.</span>
-        <span>58%</span>
+        <span>Você completou <span className="text-zinc-100">{data?.completed}</span> de <span className="text-zinc-100">{data?.total}</span> metas nessa semana.</span>
+        <span>{completedPercentage}%</span>
       </div>
       <Separator />
       
