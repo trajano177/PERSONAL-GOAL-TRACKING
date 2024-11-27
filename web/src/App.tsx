9aa7f77a458036ce02c9ff569f23af1b1552002f
Matheus Trajano <111097487+trajano177@users.noreply.github.com>
2024-11-27 20:53:52 -0300

    commit

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,29 +1,22 @@
-
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/ui/create-goal'
 import { Summary } from './components/summary'
+import { useQuery } from '@tanstack/react-query'
 import { EmptyGoals } from './components/empty-goals'
-import { useQuery} from '@tanstack/react-query'
 import { getSummary } from './http/get-summary'
+// import { EmptyGoals } from './components/empty-goals'
 
- export  function App() {
-    const {data} = useQuery({
-      queryKey: ['summary'],
-      queryFn: getSummary,
-      staleTime: 1000 * 60,
-    })
+export function App() {
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
 
-
- return (
- <Dialog>
-
-  {data?.total && data.total > 0 ?  <Summary /> : <EmptyGoals /> }
- 
-  <CreateGoal />
- </Dialog>
-)
+  return (
+    <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+      <CreateGoal />
+    </Dialog>
+  )
 }
-
-// Summary
-
-// <EmptyGoals /
--- web/src/http/get-summary.ts
+++ web/src/http/get-summary.ts
@@ -1,25 +1,19 @@
-import { dataTagSymbol } from "@tanstack/react-query";
-
-type SummaryResponse =  {
-  completed: number;
-  total: number;
-  goalsPerDay: Record<string, {
-      id: string;
-      title: string;
-      completedAt: string;
-  }[]
-  >;
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
 }
 
-
-  export async function getSummary(): Promise<SummaryResponse> {
+export async function getSummary(): Promise<SummaryResponse> {
   const response = await fetch('http://localhost:3333/summary')
-  const data =  response.json()  
-  console.log(data);
-
-  if (!response.ok) {
-    throw new Error('Failed to fetch summary');
-  }
-  
+  const data = await response.json()
+  return data.summary
 }
 
